675f232a63e1d6183697f0f27e64c8423f5432e7
ravikumar <uravikumar@outlook.com>
2018-12-24 21:06:35 +0530

    add init.pp

---
+++ manifests/init.pp
@@ -0,0 +1,13 @@
+class ntp_setup {
+  ensure_packages(['ntp'])
+    file { '/etc/ntp.conf':
+    source => 'puppet:///modules/pbg_ntp/ntp.conf',
+    notify => Service['ntp'],
+    require => Package['ntp'],
+  }
+  
+  service { 'ntp':
+    ensure => running,
+    enable => true,
+  }
+}
